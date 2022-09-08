a1c4049c20c5eedd249561e7d9123ad8ae2d3c1e
angurgapi <sophiekovalsky@gmail.com>
2022-09-08 14:37:05 +0300

    icon size fix

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
   <div class="navbar f-row">
     <div class="navbar__left f-row">
       <nuxt-link class="navbar__link" to="/">
-        <svg-icon name="home" height="0" width="20" />bl0g0sphere
+        <svg-icon name="home" height="20" width="20" />bl0g0sphere
       </nuxt-link>
     </div>
     <div class="navbar__right f-row">
